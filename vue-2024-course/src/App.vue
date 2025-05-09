4a3e783b7644910f33a524c8df066b43955ef251
Egormity <kotlaregor9@gmail.com>
2025-05-09 12:27:12 +0700

    creating new components

--- vue-2024-course/src/App.vue
+++ vue-2024-course/src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <!--  -->
 <template>
   <Navbar />
+  <Hero
+    title="Become a Vue dev"
+    subtitle="Find the Vue job that fits your skills and needs"
+  />
+  <HomeCards />
+  <JobListings limit="3" :showButton="true" />
 </template>
---
+++ vue-2024-course/src/components/JobListings.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, defineProps } from "vue";
+import jobsData from "@/jobs.json";
+import JobListing from "./JobListing.vue";
+
+//
+const jobs = ref(jobsData);
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<!--  -->
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="m-auto container-xl lg:container">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+
+      <!--  -->
+      <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
+        <JobListing
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
+      </div>
+    </div>
+  </section>
+
+  <!--  -->
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
+  </section>
+</template>
